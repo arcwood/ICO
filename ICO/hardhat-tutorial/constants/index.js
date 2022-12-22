077240ca230fb08a94a9f8abc1c63e9004206406
Archit Gangwal <gangwalarchit212@gmail.com>
2022-12-22 14:28:54 +0530

    first commit

---
+++ ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xEFe87D160261e13735918BB604B4D76d975693E8";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
